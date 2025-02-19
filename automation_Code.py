3cf0e377ccec7313b9678b92b91b5bdfb3fd4e74
ericdodoo <ericdodoo@yahoo.com>
2025-02-19 21:25:28 +0000

    add a new file

---
+++ automation_Code.py
@@ -0,0 +1,9 @@
+[
+{"id": 3, "name": "Bravo"},
+{"id": 5, "name": "Marry"},
+{"id": 6, "name": "Peter"},
+{"id": 7, "name": "Milton"},
+{"id": 8, "name": "Kwame"},
+{"id": 9, "name": "Eric"},
+{"id": 10, "name": "Pat"}
+]
